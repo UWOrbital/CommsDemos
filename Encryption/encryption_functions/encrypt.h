0bdb7c68dd021aae2357d61ff80a6217450e3b3e
Aryan Kalia <kalia.aryan2013@gmail.com>
2023-05-07 17:02:23 -0400

    tinyaes and folder restructuring

--- Encryption/encryption_functions/encrypt.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENCRYPT_H   /* Include guard */
-#define ENCRYPT_H
-
-void decrypt_new_message(const char *encrypted_message, int len, unsigned char *key,
-  unsigned char *iv, unsigned char *plaintext);
-
-#endif // ENCRYPT_H
